86d79819c223f738ed45b25d25747c6756832fbc
Sören Steiger <soren1@gmx.net>
2021-11-11 16:33:34 +0100

    Get commit messages from whatthecommit.com

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 import { logger } from './utils'
 const path = require('path')
+const request = require('request')
 
 const PORT = process.env.PORT || 5000
 
@@ -19,32 +20,21 @@ app.get('*', function (req: Request, res: Response) {
   return res.sendStatus(403)
 })
 
-const tweets = [
-  'Hacking away!',
-  'Coding, coding coding!',
-  'Add, commit & push!',
-  'Mergy, mergy!',
-  'Solved it!',
-  'Initial commit!',
-  'Final version 2',
-  'merge hell avoided',
-  'Complete rewrite (lol)',
-  'Fix some erros. Idk why the hell I did it the way I did.',
-  'Atomic commit before I fuck up more sh*t',
-  `And a commit that I don't know the reason of...`,
-  'This is not the commit message you are looking for',
-  'Dirty hack, have a better idea?',
-  'Crap. Tonight is raid night and I am already late.',
-  'Never Run This Commit As Root',
-  'For the sake of my sanity, just ignore this...',
-  'I was wrong...'
-]
-
 app.post('/post', function (req: Request, res: Response) {
   const { headers, params, query, body } = req
+
+  // github webhook
   const payload = JSON.parse(body.payload)
+
+  //console.log({ payload })
   //logger.info(payload)
-  console.log({ payload })
+
+  request('http://whatthecommit.com/index.txt', function (error, res, body) {
+    if (!error && res.statusCode == 200) {
+      const commitMessage = body.trim()
+      console.log({ commitMessage })
+    }
+  })
   return res.sendStatus(200)
 })
 
