0d9ba25992f246a581bc033c112746968e447fd6
Sören Steiger <soren1@gmx.net>
2021-11-14 12:44:12 +0100

    Integrate twitter api and send tweet on endpoint call

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import { TwitterApi } from 'twitter-api-v2'
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
 import { logger } from './utils'
 const path = require('path')
 const request = require('request')
 
-const PORT = process.env.PORT || 475
+// https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/examples.md
+const twitterClient = new TwitterApi({
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_KEY_SECRET,
+  accessToken: process.env.TWITTER_APP_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_APP_ACCESS_TOKEN_SECRET
+})
+
+const readOnlyClient = twitterClient.readOnly
+const readWriteClient = twitterClient.readWrite
+
+const PORT = process.env.PORT || 5000
 
 // create express app
 const app = express()
@@ -16,29 +30,38 @@ app.use(bodyParser.json())
 // static public files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('*', function (req: Request, res: Response) {
+app.get('*', (req: Request, res: Response) => {
   return res.sendStatus(403)
 })
 
-app.post('/post', function (req: Request, res: Response) {
+app.post('/post', async (req: Request, res: Response) => {
   const { headers, params, query, body } = req
 
   // github webhook
-  const payload = JSON.parse(body.payload)
+  //const payload = JSON.parse(body.payload)
 
   //console.log({ payload })
   //logger.info(payload)
 
-  request('http://whatthecommit.com/index.txt', function (error, res, body) {
-    if (!error && res.statusCode == 200) {
-      const commitMessage = body.trim()
-      console.log({ commitMessage })
-    }
-  })
+  try {
+    logger.info('fetching message from whatthecommit.com')
+    request('http://whatthecommit.com/index.txt', async (error, res, body) => {
+      if (!error && res.statusCode == 200) {
+        const commitMessage = body.trim()
+        logger.info(`got message: ${commitMessage}`)
+        logger.info('tweeting message')
+        await readWriteClient.v1.tweet(commitMessage)
+        logger.info('tweeting message: Success')
+      }
+    })
+  } catch (e) {
+    logger.error(e.message)
+  }
+
   return res.sendStatus(200)
 })
 
 // start express app
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), () => {
   console.log('App is running on port', app.get('port'))
 })
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv'
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { createLogger, format, transports } = require('winston')
 
 // https://www.npmjs.com/package/winston
